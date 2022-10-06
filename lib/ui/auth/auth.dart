7270c981b1d61ea0d018d208b5af68dd8cd38fda
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-10-07 00:02:14 +0330

    create home screans dart and all folder repository

---
+++ lib/ui/auth/auth.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthScrean extends StatelessWidget {
+  const AuthScrean({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
