8f807f0c03f05d97cc7ec2939d726ff935d5c268
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-10-08 00:53:54 +0330

    fixed problems on Banner Slider

--- lib/ui/widgets/slider.dart
+++ lib/ui/widgets/slider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -8,42 +7,43 @@ class BannerSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: Stack(
-        children: [
-          PageView.builder(
-              controller: _controller,
-              itemCount: 2,
-              itemBuilder: (context, index) {
-                final baneeerLocal = [
-                  "assets/img/men.png",
-                  "assets/img/women.png"
-                ];
-                return _Slider(imageAddres: baneeerLocal[index]);
-              }),
-          Positioned(
-              top: 120,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              child: Center(
-                child: SmoothPageIndicator(
-                  controller: _controller,
-                  count: 2,
-                  axisDirection: Axis.horizontal,
-                  effect: ExpandingDotsEffect(
-                      spacing: 10.0,
-                      radius: 4.0,
-                      dotWidth: 10.0,
-                      dotHeight: 10.0,
-                      paintStyle: PaintingStyle.fill,
-                      strokeWidth: 1.5,
-                      dotColor: Colors.white,
-                      activeDotColor: Theme.of(context).colorScheme.primary),
-                ),
-              ))
-        ],
+    return Center(
+      child: AspectRatio(
+        aspectRatio: 2.2,
+        child: Stack(
+          children: [
+            PageView.builder(
+                controller: _controller,
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  final baneeerLocal = [
+                    "assets/img/women.png",
+                    "assets/img/men.png"
+                  ];
+                  return _Slider(imageAddres: baneeerLocal[index]);
+                }),
+            Positioned(
+                right: 0,
+                bottom: 30,
+                left: 0,
+                child: Center(
+                  child: SmoothPageIndicator(
+                    controller: _controller,
+                    count: 2,
+                    axisDirection: Axis.horizontal,
+                    effect: ExpandingDotsEffect(
+                        spacing: 10.0,
+                        radius: 4.0,
+                        dotWidth: 10.0,
+                        dotHeight: 10.0,
+                        paintStyle: PaintingStyle.fill,
+                        strokeWidth: 1.5,
+                        dotColor: Colors.white,
+                        activeDotColor: Theme.of(context).colorScheme.primary),
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
@@ -61,7 +61,7 @@ class _Slider extends StatelessWidget {
         margin: const EdgeInsets.all(16),
         child: Image.asset(
           imageAddres,
-          fit: BoxFit.fill,
+          fit: BoxFit.cover,
         ));
   }
 }
